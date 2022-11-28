8b07c4f79b6240b2b8ddc5938deff0aecd305c3d
VictorMFL <vm859222@gmail.com>
2022-11-28 12:23:33 -0300

    Ajustando códigos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hashtag = '#'
 let caracteres = '0123456789abcdef'
+const ambosCaracteres = '0123456789abcdef'
 const numeros = '0123456789'
 const letras = 'abcdef'
 const tamanho = 6
@@ -12,11 +13,6 @@ let botaoAtualizar = document.querySelector('button')
 function geradorDeCores() {
     let cor = ''
 
-    for (let i = 0; i < tamanho; i++) {
-        const randonNumber = Math.floor(Math.random() * caracteres.length)
-        cor += caracteres.substring(randonNumber, randonNumber + 1)
-    }
-
     let numerosChecked = document.querySelector('input#inumeros')
     let letrasChecked = document.querySelector('input#iletras')
     let ambosChecked = document.querySelector('input#iambos')
@@ -25,9 +21,15 @@ function geradorDeCores() {
         caracteres = numeros
     } else if (letrasChecked.checked) {
         caracteres = letras
-    } else if (ambosChecked.checked) {
-        caracteres = caracteres
+    } else if(ambosChecked.checked) {
+        caracteres = ambosCaracteres
     }
+
+    for (let i = 0; i < tamanho; i++) {
+        const randonNumber = Math.floor(Math.random() * caracteres.length)
+        cor += caracteres.substring(randonNumber, randonNumber + 1)
+    }
+
     inputEl.value = `${hashtag}` + cor
     console.log(cor)
 }
